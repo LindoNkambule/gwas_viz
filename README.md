593dc97b9fb926f2552e9dc52af1aaaba93db8cf
LindoNkambule <lindonkambule116@gmail.com>
2020-04-07 16:34:11 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# gwaRs
+an R shiny web application for visualizing genome-wide association studies (GWAS) data using the following plot:
+
+* SNP Desnity
+* Qauntile-quantile (QQ)
+* Manhattan
+* Principal Component Analysis
+
+The application is available at [gwaRs](https://gwasviz.shinnyapps.io/gwars)
+
+## Test data
+If you want to test gwaRs, you can use the `getData.sh` script to download the test data and all required software to perform the analysis.
+
+## Reporting bugs and improvements
+If you have encountered a bug or have recommendations on how gwaRs can be improved, please report it [here](https://github.com/LindoNkambule/gwaRs/issues)
---
+++ getData.sh
@@ -0,0 +1,28 @@
+#!/usr/local/bash
+
+#1. Download PLINK
+wget http://s3.amazonaws.com/plink1-assets/plink_linux_x86_64_20200219.zip
+unzip plink_linux_x86_64_20200219.zip
+sudo cp -v plink /usr/local/bin/
+rm toy.* LICENSE prettify plink_linux_x86_64_20200219.zip
+
+#2. download test data
+wget https://www.cog-genomics.org/static/bin/plink/example.zip
+unzip example.zip
+mv example/wgas1* .
+rm -rf example example.zip # remove the folder and zip file
+
+#3. set variables
+plink="/usr/local/bin/plink2"
+output="gwaRs_QC"
+
+# the below instructions are described here: http://zzz.bwh.harvard.edu/plink/res.shtml (http://zzz.bwh.harvard.edu/plink/dist/teaching.zip)
+
+#4. CONVERT MAP/PED FILES and QAULITY CONTROL
+${plink} --file wgas1 --maf 0.01 --hwe 0.001 --geno 0.05 --mind 0.05 --recode --make-bed --out ${output} --noweb
+
+#5. Association Analysis
+${plink} --bfile gwaRs_QC --assoc --out ${output} --noweb
+
+#6. Run Principal Component Analysis
+${plink} --bfile gwaRs_QC --pca --out ${output} --noweb
